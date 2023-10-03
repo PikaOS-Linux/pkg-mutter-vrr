67fafcddb847bee57e71440a61504e5f2293b827
Ward Nakchbandi (From Fusion-voyager-3) <hotrod.master@hotmail.com>
2023-10-03 20:20:16 +0300

    commit

--- main.sh
+++ main.sh
@@ -1,15 +1,16 @@
 DEBIAN_FRONTEND=noninteractive
 
 # Clone Upstream
-git clone https://gitlab.gnome.org/GNOME/mutter -b 44.2 mutter-vrr 
+git clone https://gitlab.gnome.org/GNOME/mutter -b 45.0 mutter-vrr 
 cp -rvf ./debian ./mutter-vrr
 cd ./mutter-vrr
+for i in $(cat ../patches/series) ; do echo "Applying Patch: $i" && patch -Np1 -i ../patches/$i || bash -c "echo "Applying Patch $i Failed!" && exit 2"; done
 
 # Get build deps
 apt-get build-dep ./ -y
 
 # Build package
-LOGNAME=root dh_make --createorig -y -l -p mutter-vrr_44.2 || true
+LOGNAME=root dh_make --createorig -y -l -p mutter-vrr_45.0 || true
 dpkg-buildpackage --no-sign
 
 # Move the debs to output
